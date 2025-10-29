d514ec709bc60a27e64e03e0b3fd5e770aaa18ed
Joshua <127633954+Chaotix0@users.noreply.github.com>
2025-10-29 12:21:03 -0400

    perlin noise line and collision are connected and move with each other

--- Assets/Scripts/PerlinNoise.cs
+++ Assets/Scripts/PerlinNoise.cs
@@ -9,6 +9,7 @@ public class PerlinNoise : MonoBehaviour
     public float xScale = 1.0f;
 
     public List<Vector2> colliderPoints;
+    public PolygonCollider2D Points;
 
     private Vector2 start;
     private Vector2 end;
@@ -22,18 +23,23 @@ public class PerlinNoise : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2[] perlinToCollision = Points.points;
         for (int pn = 2; pn < colliderPoints.Count - 2; pn++)
         {
             float height = heightScale * Mathf.PerlinNoise1D(colliderPoints[pn].x * xScale);
             Vector2 newPoint = colliderPoints[pn];
             newPoint.y = height;
             colliderPoints[pn] = newPoint;
+            perlinToCollision[pn] = newPoint;
+
 
             //Vector3 pos = transform.position;
             //pos.y = height;
             //transform.position = pos;
-        }
 
+
+        }
+        Points.points = perlinToCollision;
         //if (pn == colliderPoints[16].x)
         //{
         DrawLevel(colliderPoints);
